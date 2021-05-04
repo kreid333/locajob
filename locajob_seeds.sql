5e541c5e55cd5b2e3b100b59beadc1a34f9164d0
kreid333 <kai.j.reid@gmail.com>
2021-05-04 16:33:56 -0400

    added seed file for database

---
+++ locajob_seeds.sql
@@ -0,0 +1,18 @@
+CREATE TABLE categories (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE jobs (
+    id INT NOT NULL AUTO_INCREMENT,
+    category_id INT NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    job_title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    salary VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    contact_user VARCHAR(255) NOT NULL,
+    contact_email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
