69bc35cf96d7559b13abe7b9a530eb19ce563d56
kreid333 <kai.j.reid@gmail.com>
2021-05-07 13:43:49 -0400

    added insert statements to seed file

--- locajob_seeds.sql
+++ locajob_seeds.sql
@@ -19,3 +19,11 @@ CREATE TABLE jobs (
 
 INSERT INTO categories (name)
 VALUES ("Business"), ("Technology"), ("Retail"), ("Construction");
+
+INSERT INTO jobs (category_id, company, job_title, description, salary, location, contact_user, contact_email)
+VALUES (1, "JP Morgan", "Senior Investor", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", 
+500000, "Manhattan, New York", "John Overton", "johnoover@gmail.com");
+
+INSERT INTO jobs (category_id, company, job_title, description, salary, location, contact_user, contact_email)
+VALUES (2, "Tech Guy", "Entry Level Developer", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", 
+40000, "Atlanta, Georgia", "John Doe", "johndoe@gmail.com");
