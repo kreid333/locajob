2f7a6c653374feddac550e2c99d1e5336681a34c
kreid333 <kai.j.reid@gmail.com>
2021-05-10 16:21:52 -0400

    added the ability to filter jobs by category

--- lib/Job.php
+++ lib/Job.php
@@ -18,4 +18,37 @@ class Job
 
         return $results;
     }
+
+    public function getCategories()
+    {
+        $this->db->query("SELECT * FROM categories");
+
+        // assign result set
+        $results = $this->db->resultSet();
+
+        return $results;
+    }
+
+    // get jobs by category
+    public function getByCategory($category)
+    {
+        $this->db->query("SELECT jobs.*, categories.name AS cname FROM jobs INNER JOIN categories ON jobs.category_id = categories.id WHERE jobs.category_id = $category ORDER BY post_date DESC");
+
+        // assign result set
+        $results = $this->db->resultSet();
+
+        return $results;
+    }
+
+    public function getCategory($category_id)
+    {
+        $this->db->query("SELECT * FROM categories WHERE id = :category_id");
+
+        $this->db->bind(":category_id", $category_id);
+
+        // assign result set
+        $row = $this->db->single();
+
+        return $row;
+    }
 }
--- templates/frontpage.php
+++ templates/frontpage.php
@@ -4,23 +4,34 @@
 
         <div class="p-5 mb-4 bg-light rounded-3">
             <div class="container-fluid py-5">
-                <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-                <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-                <button class="btn btn-primary btn-lg" type="button">Example button</button>
+                <h1 class="display-5 fw-bold">Find A Job</h1>
+                <form method="GET" action="index.php">
+                    <select name="category" class="form-control">
+                        <option value="0">Choose Category</option>
+                        <?php foreach ($categories as $category) : ?>
+                            <option value="<?php echo $category->id; ?>"><?php echo $category->name; ?></option>
+
+                        <?php endforeach; ?>
+                    </select>
+                    <br/>
+                    <button type="submit" class="btn btn-lg btn-success">FIND</button>
+                </form>
             </div>
         </div>
 
-        <?php foreach($jobs as $job): ?>
+        <h3><?php echo $title; ?></h3>
 
-        <div class="row">
-            <div class="col-md-10">
-                <h2><?php echo $job->job_title; ?></h2>
-                <p><?php echo $job->description; ?></p>
+        <?php foreach ($jobs as $job) : ?>
+
+            <div class="row">
+                <div class="col-md-10">
+                    <h2><?php echo $job->job_title; ?></h2>
+                    <p><?php echo $job->description; ?></p>
+                </div>
+                <div class="col-md-2">
+                    <button class="btn btn-success" type="button">Example button</button>
+                </div>
             </div>
-            <div class="col-md-2">
-                <button class="btn btn-success" type="button">Example button</button>
-            </div>
-        </div>
 
         <?php endforeach ?>
 
