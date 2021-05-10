29f40dedb867bc35ecab8f347b7ae7d569225f1c
kreid333 <kai.j.reid@gmail.com>
2021-05-10 12:51:15 -0400

    added front page template and includes folder with footer and header

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,1 +1,50 @@
-FRONT PAGE
+<?php include_once "includes/header.php" ?>
+<main>
+    <div class="container">
+
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+                <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                <button class="btn btn-primary btn-lg" type="button">Example button</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-10">
+                <h2>Change the background</h2>
+                <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-success" type="button">Example button</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10">
+                <h2>Change the background</h2>
+                <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-success" type="button">Example button</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10">
+                <h2>Change the background</h2>
+                <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-success" type="button">Example button</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10">
+                <h2>Change the background</h2>
+                <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-success" type="button">Example button</button>
+            </div>
+        </div>
+</main>
+<?php include_once "includes/footer.php" ?>
---
+++ templates/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="p-3 mt-4 text-center border-top">
+    <span>&copy; 2021</span>
+</footer>
+</body>
+
+</html>
