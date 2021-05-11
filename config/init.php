b92f4a6da6ebceb21fa3d3548d8b9b5c01c78613
kreid333 <kai.j.reid@gmail.com>
2021-05-11 16:53:18 -0400

    created the page to create the job listing and save it to the database; created helper file for redirecting and alerting the user; created create.php which defines a new page template using job-create.php; added create function to Job class; required helper in init file; included alert in header

--- config/init.php
+++ config/init.php
@@ -1,6 +1,12 @@
 <?php
+// start session
+session_start();
+
 // config file
 require_once "config.php";
+
+// require helpers
+require_once "helpers/system_helper.php";
 
 // autoloader
 spl_autoload_register(function ($class_name){
